f368a9b181bf5ce7689d8e3a138890f2de19b232
Waheed Ahmed <waheed.ahmed@arbisoft.com>
2016-05-05 15:37:34 +0500

    Account settings navigation. ECOM-2981

--- lms/static/js/student_account/views/account_settings_fields.js
+++ lms/static/js/student_account/views/account_settings_fields.js
@@ -1,12 +1,43 @@
 ;(function (define, undefined) {
     'use strict';
     define([
-        'gettext', 'jquery', 'underscore', 'backbone', 'js/views/fields'
-    ], function (gettext, $, _, Backbone, FieldViews) {
+        'gettext',
+        'jquery',
+        'underscore',
+        'backbone',
+        'js/views/fields',
+        'text!templates/fields/field_text_account.underscore',
+        'text!templates/fields/field_readonly_account.underscore',
+        'text!templates/fields/field_link_account.underscore',
+        'text!templates/fields/field_dropdown_account.underscore',
+        'text!templates/fields/field_social_link_account.underscore'
+    ], function (
+        gettext, $, _, Backbone,
+        FieldViews,
+        field_text_account_template,
+        field_readonly_account_template,
+        field_link_account_template,
+        field_dropdown_account_template,
+        field_social_link_template)
+    {
 
         var AccountSettingsFieldViews = {};
 
+        AccountSettingsFieldViews.ReadonlyFieldView = FieldViews.ReadonlyFieldView.extend({
+            fieldTemplate: field_readonly_account_template
+        });
+
+        AccountSettingsFieldViews.TextFieldView = FieldViews.TextFieldView.extend({
+            fieldTemplate: field_text_account_template
+        });
+
+        AccountSettingsFieldViews.DropdownFieldView = FieldViews.DropdownFieldView.extend({
+            fieldTemplate: field_dropdown_account_template
+        });
+
         AccountSettingsFieldViews.EmailFieldView = FieldViews.TextFieldView.extend({
+
+            fieldTemplate: field_text_account_template,
 
             successMessage: function() {
                 return this.indicators.success + interpolate_text(
@@ -20,6 +51,8 @@
         });
 
         AccountSettingsFieldViews.LanguagePreferenceFieldView = FieldViews.DropdownFieldView.extend({
+
+            fieldTemplate: field_dropdown_account_template,
 
             saveSucceeded: function () {
                 var data = {
@@ -47,6 +80,8 @@
         });
 
         AccountSettingsFieldViews.PasswordFieldView = FieldViews.LinkFieldView.extend({
+
+            fieldTemplate: field_link_account_template,
 
             initialize: function (options) {
                 this.options = _.extend({}, options);
@@ -88,7 +123,7 @@
             }
         });
 
-        AccountSettingsFieldViews.LanguageProficienciesFieldView = FieldViews.DropdownFieldView.extend({
+        AccountSettingsFieldViews.LanguageProficienciesFieldView = AccountSettingsFieldViews.DropdownFieldView.extend({
 
             modelValue: function () {
                 var modelValue = this.model.get(this.options.valueAttribute);
@@ -111,6 +146,12 @@
 
         AccountSettingsFieldViews.AuthFieldView = FieldViews.LinkFieldView.extend({
 
+            fieldTemplate: field_social_link_template,
+
+            className: function () {
+                return 'u-field u-field-social u-field-' + this.options.valueAttribute;
+            },
+
             initialize: function (options) {
                 this.options = _.extend({}, options);
                 this._super(options);
@@ -118,13 +159,22 @@
             },
 
             render: function () {
-                var linkTitle;
+                var linkTitle = '',
+                    linkClass = '',
+                    subTitle = '';
                 if (this.options.connected) {
-                    linkTitle = gettext('Unlink');
+                    linkTitle = gettext('Unlink This Account');
+                    linkClass = 'social-field-linked';
+                    subTitle = gettext(
+                        'You can use your ' + this.options.title + ' account to sign in to your edX account.'
+                    );
                 } else if (this.options.acceptsLogins) {
-                    linkTitle = gettext('Link')
-                } else {
-                    linkTitle = ''
+                    linkTitle = gettext('Link Your Account');
+                    linkClass = 'social-field-unlinked';
+                    subTitle = gettext(
+                        'Link your ' + this.options.title + ' account to your edX account and use ' +
+                        this.options.title + ' to sign in to edX.'
+                    );
                 }
 
                 this.$el.html(this.template({
@@ -132,7 +182,9 @@
                     title: this.options.title,
                     screenReaderTitle: this.options.screenReaderTitle,
                     linkTitle: linkTitle,
-                    linkHref: '',
+                    subTitle: subTitle,
+                    linkClass: linkClass,
+                    linkHref: '#',
                     message: this.helpMessage
                 }));
                 return this;
