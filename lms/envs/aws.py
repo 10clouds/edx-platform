cdb345889e7705965a20eced53c14c660536dbde
inlanger <inlanger@gmail.com>
2016-08-14 19:19:32 +0300

    EDEVATE_BASE_URL fix

--- lms/envs/aws.py
+++ lms/envs/aws.py
@@ -820,4 +820,4 @@ EDEVATE_MYSQL_USER = 'edevate_devel'
 EDEVATE_MYSQL_PASSWD = 'edevate12'
 EDEVATE_MYSQL_DB_NAME = 'edevate_devel'
 
-EDEVATE_BASE_URL = 'http://50.97.47.83:18000/'
+EDEVATE_BASE_URL = 'http://ec2-23-22-165-116.compute-1.amazonaws.com/'
