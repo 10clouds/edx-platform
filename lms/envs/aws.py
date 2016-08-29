a2be1c19487ffaa415220ce6440f0f1143421149
inlanger <inlanger@gmail.com>
2016-08-29 16:08:07 +0300

    change CAS settings

--- lms/envs/aws.py
+++ lms/envs/aws.py
@@ -324,7 +324,8 @@ if FEATURES.get('AUTH_USE_CAS'):
         'django_cas.backends.CASBackend',
     )
     INSTALLED_APPS += ('django_cas',)
-    MIDDLEWARE_CLASSES += ('django_cas.middleware.CASMiddleware',)
+    MIDDLEWARE_CLASSES += ('django_cas.middleware.CASMiddleware',
+                           'django.middleware.doc.XViewMiddleware')
     CAS_ATTRIBUTE_CALLBACK = ENV_TOKENS.get('CAS_ATTRIBUTE_CALLBACK', None)
     if CAS_ATTRIBUTE_CALLBACK:
         import importlib
