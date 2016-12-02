fb368eaaae8e20a4104688cd6001e17d9c7c2887
inlanger <inlanger@gmail.com>
2016-12-02 14:10:49 +0200

    Delete both email and password fields from account settings page

--- lms/static/js/student_account/views/account_settings_factory.js
+++ lms/static/js/student_account/views/account_settings_factory.js
@@ -45,29 +45,29 @@
                                 persistChanges: true
                             })
                         },
-                        {
-                            view: new AccountSettingsFieldViews.EmailFieldView({
-                                model: userAccountModel,
-                                title: gettext('Email Address'),
-                                valueAttribute: 'email',
-                                helpMessage: interpolate_text(
-                                    gettext('The email address you use to sign in. Communications from {platform_name} and your courses are sent to this address.'), {platform_name: platformName}
-                                ),
-                                persistChanges: true
-                            })
-                        },
-                        {
-                            view: new AccountSettingsFieldViews.PasswordFieldView({
-                                model: userAccountModel,
-                                title: gettext('Password'),
-                                screenReaderTitle: gettext('Reset your Password'),
-                                valueAttribute: 'password',
-                                emailAttribute: 'email',
-                                linkTitle: gettext('Reset Password'),
-                                linkHref: fieldsData.password.url,
-                                helpMessage: gettext('When you click "Reset Password", a message will be sent to your email address. Click the link in the message to reset your password.')
-                            })
-                        },
+                        // {
+                        //     view: new AccountSettingsFieldViews.EmailFieldView({
+                        //         model: userAccountModel,
+                        //         title: gettext('Email Address'),
+                        //         valueAttribute: 'email',
+                        //         helpMessage: interpolate_text(
+                        //             gettext('The email address you use to sign in. Communications from {platform_name} and your courses are sent to this address.'), {platform_name: platformName}
+                        //         ),
+                        //         persistChanges: true
+                        //     })
+                        // },
+                        // {
+                        //     view: new AccountSettingsFieldViews.PasswordFieldView({
+                        //         model: userAccountModel,
+                        //         title: gettext('Password'),
+                        //         screenReaderTitle: gettext('Reset your Password'),
+                        //         valueAttribute: 'password',
+                        //         emailAttribute: 'email',
+                        //         linkTitle: gettext('Reset Password'),
+                        //         linkHref: fieldsData.password.url,
+                        //         helpMessage: gettext('When you click "Reset Password", a message will be sent to your email address. Click the link in the message to reset your password.')
+                        //     })
+                        // },
                         {
                             view: new AccountSettingsFieldViews.LanguagePreferenceFieldView({
                                 model: userPreferencesModel,
